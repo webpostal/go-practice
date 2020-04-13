2fc1c3d3ea3b63b53b148dc0704505246f2f8771
webpostal <webpostal@gmail.com>
2020-04-13 12:39:40 -0400

    ex,.switch statements

---
+++ 008-control-flow/5-Switch Statements.go
@@ -0,0 +1,41 @@
+package main
+
+import "fmt"
+
+func main() {
+
+	switch {
+
+	case (2 == 2):
+		fmt.Println("Eval to true")
+	case (2 == 6):
+		fmt.Println(false)
+
+	}
+
+	switch "Rossina" {
+
+	case "Camila":
+		fmt.Println("Camila will not Print")
+	case "Georgina":
+		fmt.Println("Georgina will not Print")
+	case "Julianna", "Rossina":
+		fmt.Println("Rossina will Print")
+	default:
+		fmt.Println("Default value")
+	}
+
+	x := "River"
+	switch x {
+
+	case "Mountain":
+		fmt.Println("Mountain will not Print")
+	case "River", "Kwai":
+		fmt.Println("The Bridge on the River Kwai")
+	case "Lake":
+		fmt.Println("Rossina will Print")
+	default:
+		fmt.Println("Default value")
+	}
+
+}
